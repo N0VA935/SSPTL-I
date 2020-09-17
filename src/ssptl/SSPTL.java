2eebc9a54da87c0055d182df4d72f2b12d24f234
Jose David Delgado Ramirez <huevo@192.168.1.79>
2020-09-17 00:07:26 -0500

    Actualizacion: Lee el archivo como se tiene

--- src/ssptl/SSPTL.java
+++ src/ssptl/SSPTL.java
@@ -5,6 +5,11 @@
  */
 package ssptl;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  *
  * @author José David Delgado Ramírez
@@ -14,8 +19,17 @@ public class SSPTL {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void reader(String txt) throws FileNotFoundException, IOException{
+        String line;
+        FileReader fr = new FileReader(txt);
+        BufferedReader br = new BufferedReader(fr);
+        while((line = br.readLine())!= null){
+            System.out.println(line);
+        }
+        fr.close();
+    }
+    public static void main(String[] args) throws IOException {
+        reader("src/txt/P1ASM.txt");
     }
     
 }
