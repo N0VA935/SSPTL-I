5675ab509d1895b530dbc991425a0875d2d8a169
Jose David Delgado Ramirez <huevo@192.168.1.79>
2020-09-19 19:38:31 -0500

    Actualizacion: Separa de acuerdo a las etiquetas, CODOP y operando Falla: Impresion de resultados en orden

--- src/ssptl/SSPTL.java
+++ src/ssptl/SSPTL.java
@@ -9,6 +9,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  *
@@ -16,20 +17,68 @@ import java.io.IOException;
  */
 public class SSPTL {
 
+    private static ArrayList<String> TAG = new ArrayList<String>();
+    private static ArrayList<String> OPCODE = new ArrayList<String>();
+    private static ArrayList<String> OPER = new ArrayList<String>();
+    //private static boolean exit = false;
+    private static String etq = "ETIQUETA= ";
+    private static String CODOP = "CODOP= ";
+    private static String op = "OPERANDO= ";
+    
     /**
-     * @param args the command line arguments
+     * @param txt Direccion de donde se tomara el txt para la lectura
      */
     public static void reader(String txt) throws FileNotFoundException, IOException{
-        String line;
+        String line;    // Variable para leer temporalmente las lineas del txt
+        int contAux = 0;
+        // Lectores para cargar el .txt
         FileReader fr = new FileReader(txt);
         BufferedReader br = new BufferedReader(fr);
+        
         while((line = br.readLine())!= null){
-            System.out.println(line);
+                String[] instructionSet = line.split("\t");
+                for (int i = 0; i < instructionSet.length; i++) {
+                    if(instructionSet[i].matches("^\\p{Alpha}*\\p{Alnum}*") && (i == 0 && instructionSet[i].length() <= 8)) {
+                        if(instructionSet[i].isEmpty()){
+                            TAG.add(etq + "NULL");
+                        } else {
+                            TAG.add(etq + instructionSet[i]);
+                        }
+                    }else
+                        if(instructionSet[i].matches("(\\p{Alpha}*)|(\\p{Alpha}*.{0,1}\\p{Alpha}*)|(\\p{Alpha}*)") && ((i == 1) && instructionSet[i].length() <= 5)){
+                            if(instructionSet[i].isEmpty()){
+                                OPCODE.add(CODOP + "NULL");
+                            } else {
+                                OPCODE.add(CODOP + instructionSet[i]);
+                            }
+                        }else
+                            if(instructionSet[i].matches("\\p{Punct}*\\p{Alnum}*") && i==2) {
+                                if(instructionSet[i].isEmpty()){
+                                    OPER.add(op + "NULL");
+                                } else {
+                                    OPER.add(op + instructionSet[i]);
+                                }
+                            }
+            }
         }
+        
         fr.close();
     }
+    
     public static void main(String[] args) throws IOException {
         reader("src/txt/P1ASM.txt");
+        
+       
+         for (String etiqueta : TAG) {
+            System.out.println(etiqueta);
+            
+            
+        }
+        for (String codop : OPCODE) {
+                System.out.println(codop);
+        }
+        for (String op : OPER) {
+                    System.out.println(op);
+                }
     }
-    
-}
+    }
